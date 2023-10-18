3732b50b6df6c225ca6475c44900b595ff3b7edd
meoKibo <theboyhunter123@gmail.com>
2023-10-18 22:01:01 +0700

    Add project files.

---
+++ testNetWebAPI/Helper/ApplicationMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MyApiNetCore6.Data;
+using MyApiNetCore6.Models;
+
+namespace testNetWebAPI.Helper
+{
+    public class ApplicationMapper : Profile
+    {
+        public ApplicationMapper()
+        {
+            CreateMap<Book, BookModel>().ReverseMap();
+        }
+    }
+}
